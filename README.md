415e55c83404dc82a7a0bc7e361aa21287350748
Émile Larose-Levac <emile.larose@gmail.com>
2018-12-05 16:09:47 -0500

    Finished second part of day1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Advant-of-Code-2018
+# Advant of Code 2018
+My solutions for the Advant of Code 2018
+
+See https://adventofcode.com/2018 for all the info on the problems to solve.
--- day1/main.cpp
+++ day1/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <set>
+#include <algorithm>
 
 int main() {
     std::string inputFilePath = "../input.txt";
@@ -10,11 +13,34 @@ int main() {
         return 0;
     }
 
-    int resultingFrequency = 0;
-    int variation = 0;
+    int resultingFrequency = 0, variation = 0, firstFreqTwice = 0;
+    std::vector<int> allInputFreq;
+    std::set<int> allResFreq = {resultingFrequency};
+    bool foundFreqTwice = false;
+
     while(inputFile >> variation){
+        allInputFreq.push_back(variation);
         resultingFrequency += variation;
+        if(!foundFreqTwice){
+            if(!allResFreq.insert(resultingFrequency).second){ // Insert fails in set if the value already exist
+                // Found first frequency that appears twice
+                foundFreqTwice = true;
+                firstFreqTwice = resultingFrequency;
+            }
+        }
     }
-    std::cout << resultingFrequency << "\n";
+
+    int answerProblem1 = resultingFrequency;
+
+    // If no duplicate frequencies where found loop over the input frequencies and add to the resulting frequency
+    for(int i = 0; !foundFreqTwice; i++){
+        if(!allResFreq.insert(resultingFrequency += allInputFreq[i % allInputFreq.size()]).second){
+            // Found first frequency that appears twice
+            foundFreqTwice = true;
+            firstFreqTwice = resultingFrequency;
+        }
+    }
+    std::cout << "Resulting frequency : " << answerProblem1 << "\n";
+    std::cout << "First frequency found twice : " << firstFreqTwice << "\n";
     return 0;
 }
