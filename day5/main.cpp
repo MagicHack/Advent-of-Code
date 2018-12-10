7c7c05139909a7c34cab976ee8d7252bafdd9e43
Émile Larose-Levac <emile.larose@gmail.com>
2018-12-10 16:30:26 -0500

    Big speed improvement

--- day5/main.cpp
+++ day5/main.cpp
@@ -9,6 +9,7 @@
 
 int react(std::string polymer);
 int part2Thread(std::string polymer, char c);
+int reactFaster(const std::string& polymer);
 
 int main(int argc, char* argv[]) {
     std::string inputFilePath = "../input.txt";
@@ -26,7 +27,7 @@ int main(int argc, char* argv[]) {
     }
     // Part 1
     int startingUnits = polymer.size();
-    int remainingUnits = react(polymer);
+    int remainingUnits = reactFaster(polymer);
 
     std::cout << "Part 1:\n" << "\tStarting units : " << startingUnits << "\n\tRemaining units : " << remainingUnits << "\n";
 
@@ -40,7 +41,7 @@ int main(int argc, char* argv[]) {
     for(char c = 'A'; c <= 'Z'; c++){
         while(threads.size() >= maxThreads){ // Wait for a thread to finish
             for(int i = 0; i < threads.size(); i++){
-                if(threads[i].wait_for(std::chrono::milliseconds(1)) == std::future_status::ready) {
+                if(threads[i].wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
                     polymerSizes.push_back(threads[i].get());
                     threads.erase(threads.begin() + i);
                 }
@@ -63,9 +64,21 @@ int main(int argc, char* argv[]) {
 int part2Thread(std::string polymer, char c) {
     polymer.erase(std::remove(polymer.begin(), polymer.end(), c), polymer.end());
     polymer.erase(std::remove(polymer.begin(), polymer.end(), std::tolower(c)), polymer.end());
-    return react(polymer);
+    return reactFaster(polymer);
 }
-
+// Second solution : much faster - threading is kinda usuless now
+int reactFaster(const std::string& polymer){
+    std::string result = "-"; // begin with a lenght of one so [end() - 1] does not segfault
+    for(auto c: polymer) {
+        if(c != *(result.end() - 1) && tolower(c) == tolower(*(result.end() - 1))){
+            result.pop_back();
+        } else{
+            result.push_back(c);
+        }
+    }
+    return result.size() - 1;
+}
+// First solution : works but very slowly
 int react(std::string polymer){
     bool destroyed;
     do{
