ac9398c0a1eff5b178efc8a2947d61f968f4f288
Émile Larose-Levac <emile.larose@gmail.com>
2018-12-11 14:24:08 -0500

    Added threading to day11 part2

--- day11/main.cpp
+++ day11/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <set>
+#include <vector>
+#include <thread>
+#include <future>
+#include <chrono>
 
 const int GRID_SIZE = 300;
+
+struct GridPower {
+    int maxPower;
+    int x;
+    int y;
+    int size;
+};
 
 int caclcultatePower(int x, int y, int serial){
     int rackId = x + 10;
@@ -12,13 +23,34 @@ int caclcultatePower(int x, int y, int serial){
     return powerLevel - 5;
 }
 
-void printGrid(int grid[300][300]){
+void printGrid(const int grid[300][300]){
     for(int y = 0; y < GRID_SIZE; y++){
         for(int x = 0; x < GRID_SIZE; x++){
             std::cout << grid[y][x] << "\t";
         }
         std::cout << "\n";
     }
+}
+
+GridPower gridMaxPower(const int cells[GRID_SIZE][GRID_SIZE], int s){
+    int maxPower = 0;
+    std::pair<int, int> coord = std::make_pair(0, 0);
+    for(int y = 0; y < GRID_SIZE - s; y++){
+        for(int x = 0; x < GRID_SIZE - s; x++){
+            int power = 0;
+            for(int i = 0; i < s; i++){
+                for(int j = 0; j < s; j++){
+                    power += cells[y + i][x + j];
+                }
+            }
+            if(power > maxPower){
+                maxPower = power;
+                coord.first = x;
+                coord.second = y;
+            }
+        }
+    }
+    return GridPower {maxPower, coord.first, coord.second, s};
 }
 
 int main() {
@@ -31,31 +63,40 @@ int main() {
             cells[y][x] = caclcultatePower(x, y, input);
         }
     }
-    int maxPower = 0;
-    std::pair<int, int> coord;
-    int size = 0;
+
+    // Part 1
+    GridPower part1 = gridMaxPower(cells, 3);
+    std::cout << "Part 1:\n" << "\tMax power is : " << part1.maxPower << "\n\tat : " << part1.x << "," << part1.y << "\n";
+
+    // Part 2
+    // Even with threading bruteforcing is kinda slow
+    std::vector<std::future<GridPower>> threads;
+    unsigned maxThreads = std::thread::hardware_concurrency();
+    std::cout << "Number of threads : " << maxThreads << "\n";
+    GridPower currentMax = {0, 0, 0, 0};
     for(int s = 0; s <= GRID_SIZE; s++){
-        for(int y = 0; y < GRID_SIZE - s; y++){
-            for(int x = 0; x < GRID_SIZE - s; x++){
-                int power = 0;
-                for(int i = 0; i < s; i++){
-                    for(int j = 0; j < s; j++){
-                        power += cells[y + i][x + j];
+        while(threads.size() >= maxThreads){ // Wait for a thread to finish
+            for(int i = 0; i < threads.size(); i++){
+                if(threads[i].wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
+                    GridPower power = threads[i].get();
+                    if(power.maxPower > currentMax.maxPower){
+                        currentMax = power;
                     }
-                }
-                if(power > maxPower){
-                    maxPower = power;
-                    coord.first = x;
-                    coord.second = y;
-                    size = s;
+                    threads.erase(threads.begin() + i);
                 }
             }
         }
+        threads.push_back(std::async(std::launch::async, gridMaxPower, cells, s)); // Launch thread
     }
 
-
-    std::cout << "Max power is " << maxPower << " at (" << coord.first << "," << coord.second << ")\n";
-    std::cout << "Grid size is " << size << "x" << size << "\n";
+    for(auto &thread : threads) {
+        GridPower power = thread.get();
+        if(power.maxPower > currentMax.maxPower){
+            currentMax = power;
+        }
+    }
+    std::cout << "Part 2:\n" << "\tMax power is : " << currentMax.maxPower << "\n\tat : " << currentMax.x << "," << currentMax.y << "\n";
+    std::cout << "Grid size is " << currentMax.size << "x" << currentMax.size << "\n";
 
     return 0;
 }
